e8f91cccd63429407dc7e65fe8bf1e126993e2be
hajime7005 <kbc16a15@stu.kawahara.ac.jp>
2017-07-14 23:41:33 +0900

    [Del]protoは使わない感あるので消しました

--- proto/map.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-    var opts = {
-        zoom: 15,
-        center: new google.maps.LatLng(35.1253694,136.9073667)
-    };
-    var map = new google.maps.Map(document.getElementById("map"), opts);
-}
