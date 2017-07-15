9dce5d87ff47649470372d26a70a3584580d4385
Hajime Nishibe <kbc16a15@stu.kawahara.ac.jp>
2017-07-16 02:36:03 +0900

    [Add]地図に現在地を追跡する機能を追加（動作未確認）

--- MapTest/Map.js
+++ MapTest/Map.js
@@ -3,65 +3,86 @@
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        //とりまデフォ値(地図の中心点)
+        //デフォ値(地図の中心点)
         center: {lat: 33.8397463, lng: 132.7566273},
         zoom: 14
     });
     var infoWindow = new google.maps.InfoWindow({map: map});
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {    //位置情報を取得できたならデフォ値を書き換える的な
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('ボクハフジ');
-            map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-       
-    
-        $.ajax({
-            type: "get",
-            url: "ConnectDummyDB.php",
-            data: "",
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function (data) {
+    var cnt = 0;
+    var mylocation = function (){
+        if (navigator.geolocation) {    //現在地が取得できるか判定します
+            navigator.geolocation.getCurrentPosition(function(position) {
                 
-                $.each(data,function(index,val){
-                    console.log(data[index].roomID);
-                    
-                    var point = new google.maps.LatLng(
-                        data[index].latitude,
-                        data[index].longitude
-                    );
-                    
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        position: point
-                    });
-          
-                })
-                console.log(data[1].roomID);
-             }
-        });
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
 
+                //現在地にマーカを置きます
+                var myplace = new google.maps.Marker({
+                    map: map,
+                    position: pos,
+                    icon: {
+                        path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+                        scale: 6
+                    }
+                });
+                
+                if(cnt === 0){  //初回のみ現在地マーカの削除を行わない
+                    ++cnt;
+                    console.log("初回");
+                }else{
+                    myplace.setMap(null);
+                }
+                
+
+                //地図の中心を現在地に書き換えます
+                map.setCenter(pos);
+                myplace.setMap(map);
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });  
+        } else {
+            // 現在地が取得できなかったらエラーを表示する
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    };
         
         
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+    setInterval(mylocation, 5000);
+    
+    
+    //目的地を地図に表示するための処理   
+    $.ajax({
+        type: "get",
+        url: "ConnectDummyDB.php",
+        data: "",
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+
+            $.each(data,function(index,val){
+               
+                var point = new google.maps.LatLng(
+                    data[index].latitude,
+                    data[index].longitude
+                );
+
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: point
+                });
+
+            });
+         }
+    });
+
  
  
 }
 
-//ブラウザがGeolocationっていう位置情報的なのに対応してなかったらエラーだすぜみたいな
+//ブラウザがGeolocationっていう位置情報的なのに対応してなかったら出すエラーの中身
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
