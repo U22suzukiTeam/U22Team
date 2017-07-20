24f1e46a61fe75e8d7704d34df83a7808fb81594
takahashi yoshiki <kbc16a11@stu.kawahara.ac.jp>
2017-07-20 14:37:35 +0900

    entry(デザイン変更,js改良)

--- entry/js/entry.js
+++ entry/js/entry.js
@@ -2,39 +2,72 @@ function FormCheck() {
 
 	// 未入力チェック
 	var val = document.getElementsByName("accountname")[0];
-	if ( jsTrim( val.value ).length == 0 ) {
-		alert( "アカウント名は必須入力です" );
+	var val2 = document.getElementsByName("username")[0];
+	var val3 = document.getElementsByName("password")[0];
+	var val4 = document.getElementsByName("password2")[0];
+	if ( jsTrim( val.value ).length == 0 & jsTrim( val2.value ).length == 0 & jsTrim( val3.value ).length == 0 & jsTrim( val4.value ).length == 0) {
+		alert( "アカウント名は必須入力です。\nユーザーネーム（表示名）は必須入力です。\nパスワードは必須入力です。\nパスワード（再入力）は必須入力です。" );
 		val.focus();
 		return false;
-	}
-	
-	// 未入力チェック
-	var val = document.getElementsByName("username")[0];
-	if ( jsTrim( val.value ).length == 0 ) {
-		alert( "ユーザーネーム(表示名)は必須入力です" );
+	}else if( jsTrim( val.value ).length == 0 & jsTrim( val2.value ).length == 0 & jsTrim( val3.value ).length == 0 ){
+		alert( "アカウント名は必須入力です。\nユーザーネーム（表示名）は必須入力です。\nパスワードは必須入力です。" );
 		val.focus();
 		return false;
-	}
-
-	// 未入力チェック
-	var val = document.getElementsByName("password")[0];
-	if ( jsTrim( val.value ).length == 0 ) {
-		alert( "パスワードは必須入力です" );
+	}else if( jsTrim( val.value ).length == 0 & jsTrim( val2.value ).length == 0 & jsTrim( val4.value ).length == 0 ){
+		alert( "アカウント名は必須入力です。\nユーザーネーム（表示名）は必須入力です。\nパスワード（再入力）は必須入力です。" );
 		val.focus();
 		return false;
-	}
-	
-	// 未入力チェック
-	var val = document.getElementsByName("password2")[0];
-	if ( jsTrim( val.value ).length == 0 ) {
-		alert( "パスワード(再入力)は必須入力です" );
+	}else if( jsTrim( val.value ).length == 0 & jsTrim( val3.value ).length == 0 & jsTrim( val4.value ).length == 0 ){
+		alert( "アカウント名は必須入力です。\nパスワードは必須入力です。\nパスワード（再入力）は必須入力です。" );
 		val.focus();
 		return false;
-	}
-	
+	}else if( jsTrim( val.value ).length == 0 & jsTrim( val2.value ).length == 0 ){
+		alert( "アカウント名は必須入力です。\nユーザーネーム（表示名）は必須入力です。" );
+		val.focus();
+		return false;
+	}else if( jsTrim( val.value ).length == 0 & jsTrim( val3.value ).length == 0 ){
+		alert( "アカウント名は必須入力です。\nパスワードは必須入力です。" );
+		val.focus();
+		return false;
+	}else if( jsTrim( val.value ).length == 0 & jsTrim( val4.value ).length == 0 ){
+		alert( "アカウント名は必須入力です。\nパスワード（再入力）は必須入力です。" );
+		val.focus();
+		return false;
+	}else if( jsTrim( val2.value ).length == 0 & jsTrim( val3.value ).length == 0 & jsTrim( val4.value ).length == 0){
+		alert( "ユーザーネーム（表示名）は必須入力です。\nパスワードは必須入力です。\nパスワード（再入力）は必須入力です。" );
+		val.focus();
+		return false;
+	}else if( jsTrim( val2.value ).length == 0 & jsTrim( val3.value ).length == 0 ){
+		alert( "ユーザーネーム（表示名）は必須入力です。\nパスワードは必須入力です。" );
+		val.focus();
+		return false;
+	}else if( jsTrim( val2.value ).length == 0 & jsTrim( val4.value ).length == 0){
+		alert( "ユーザーネーム（表示名）は必須入力です。\nパスワード（再入力）は必須入力です。" );
+		val.focus();
+		return false;
+	}else if( jsTrim( val3.value ).length == 0 & jsTrim( val4.value ).length == 0){
+		alert( "パスワードは必須入力です。\nパスワード（再入力）は必須入力です。" );
+		val.focus();
+		return false;
+	}else if ( jsTrim( val.value ).length == 0 ) {
+		alert( "アカウント名は必須入力です。" );
+		val.focus();
+		return false;
+	}else if ( jsTrim( val2.value ).length == 0 ) {
+		alert( "ユーザーネーム(表示名)は必須入力です。" );
+		val.focus();
+		return false;
+	}else if ( jsTrim( val3.value ).length == 0 ) {
+		alert( "パスワードは必須入力です。" );
+		val.focus();
+		return false;
+	}else if ( jsTrim( val4.value ).length == 0 ) {
+		alert( "パスワード(再入力)は必須入力です。" );
+		val.focus();
+		return false;
 	//パスワードチェック
-	if (form1.password.value != form1.password2.value) {
-		alert("パスワードが一致しません");
+	}else if (form1.password.value != form1.password2.value) {
+		alert("パスワードが一致しません。");
 		return false;
 	}
 
