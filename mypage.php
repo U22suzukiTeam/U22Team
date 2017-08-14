5ec4b7b4b2525ab15772e5ea65ade8d1972af328
takahashi yoshiki <kbc16a11@stu.kawahara.ac.jp>
2017-08-14 16:28:25 +0900

    [change]デザイン変更

--- mypage.php
+++ mypage.php
@@ -8,16 +8,26 @@ session_start();
 	<meta charset="UTF-8">
 	<title>マイページ</title>
 	<link rel="stylesheet" href="mypage.css" type="text/css">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
 <!--
 	<img id="logo" src="images/logo.png" alt="待ち合わせするんです"width="400" height="80">
 -->
-	<p>ようこそ<u><?php echo htmlspecialchars($_SESSION["username"], ENT_QUOTES); ?></u>さん</p><br>
+<div class="col-xs-offset-4 col-xs-4" style="margin-top:150px;">
+  <p>ようこそ<u><?php echo htmlspecialchars($_SESSION["username"], ENT_QUOTES); ?></u>さん</p><br>
 
-<form action="createLoom.php" onsubmit="return Checkposition()">
+  <form action="createLoom.php" onsubmit="return Checkposition()">
 	<input class="button" type="submit" value="ルーム作成">
-</form>
+  </form>
+
+  <form action="Logout.php">
+	<input class="button" type="submit" value="ログアウト">
+  </form>
+</div>
 
 <script type="text/javascript">
 //位置情報が取得可能かどうか
@@ -30,10 +40,8 @@ function Checkposition(){
         }
 }
 </script>
-
-<form action="Logout.php">
-	<input class="button" type="submit" value="ログアウト">
-</form>
+<script src="https://code.jquery.com/jquery.js"></script>
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
 </body>
 </html>
